f84b0d96d7eb4dc359b5b51b5064a16eeb7df3fd
enfsl <emrenefesli@outlook.com>
2019-04-30 16:12:03 +0300

    Add Credit Card Form

--- scripts/script.js
+++ scripts/script.js
@@ -4,8 +4,7 @@ $(function() {
       $(".header").addClass("sabit-header");
       $(".nav-a").addClass("nav-a-sabit");
       $(".nav-li").addClass("nav-li-new");
-    } 
-    else {
+    } else {
       $(".header").removeClass("sabit-header");
       $(".nav-a").removeClass("nav-a-sabit");
       $(".nav-li").removeClass("nav-li-new");
