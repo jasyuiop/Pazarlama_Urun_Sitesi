a84c635628fa0996c98b07be7f2a46cfd114ef1c
enfsl <emrenefesli@outlook.com>
2019-04-30 21:56:20 +0300

    alert olayları düzenlendi

--- scripts/script.js
+++ scripts/script.js
@@ -11,3 +11,18 @@ $(function() {
     }
   });
 });
+function msgcontrol() {
+  var cdnumber = document.getElementById("cardnumber").value;
+  var uzunluk = cdnumber.length;
+  var cdowner = document.getElementById("cardowner").value;
+  var cdexpiry = document.getElementById("cardexpiry").value;
+  var cdcvc = document.getElementById("cardcvc").value;
+  if (
+    uzunluk !== 19 ||
+    cdowner.indexOf(" ") == -1 ||
+    cdexpiry.length !== 7 ||
+    cdcvc.length !== 3
+  )
+    alert("Kredi kartı bilgisi doğru değil!");
+  else alert("Satın alma başarılı");
+}
